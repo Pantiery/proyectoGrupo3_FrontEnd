0a4db30e70f72059dee084747e84c49c8638d2b3
Matias <matiaspencef@gmail.com>
2026-01-27 23:09:29 +0100

    cambios en login.js para probar login

--- js/login.js
+++ js/login.js
@@ -9,15 +9,41 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
-        // Validación simulada
-        if (email === "admin@tecticket.com" && password === "1234") {
-            alertBox.classList.add("d-none");
-            alert("✅ Login correcto");
-            // window.location.href = "dashboard.html";
-        } else {
-            alertBox.classList.remove("d-none");
-        }
-    });
+       fetch("/proyectoGrupo3_BackEnd/public/login", {
+            method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            correo: email,
+            contrasena: password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
 
+        if (!data.ok) {
+            alertBox.classList.remove("d-none");
+            return;
+        }
+
+        alertBox.classList.add("d-none");
+
+        if (data.rol === "CLIENTE") {
+            window.location.href = "/cliente/crear-ticket.html";
+        }
+
+        if (data.rol === "ADMIN") {
+            window.location.href = "/admin/panel.html";
+        }
+
+        if (data.rol === "TECNICO") {
+            window.location.href = "/tecnico/tickets.html";
+        }
+    })
+        .catch(() => {
+            alertBox.classList.remove("d-none");
+        });
+    });
 });
 
